c04d1e4d1ab122fa81938d9de78dac4dab69a450
MaFi-Games <90082857+MaFi-Games@users.noreply.github.com>
2021-09-18 09:54:44 -0700

    Basic project structure and initial modding examples

---
+++ MyIds.Recipes.cs
@@ -0,0 +1,14 @@
+﻿using Mafi.Base;
+using RecipeID = Mafi.Core.Factory.Recipes.RecipeProto.ID;
+
+namespace ExampleMod {
+	public partial class MyIds {
+
+		public partial class Recipes {
+
+			public static readonly RecipeID MySmoothSmelting = Ids.Recipes.CreateId("MySmoothSmelting");
+
+		}
+
+	}
+}
---
+++ MyIds.Research.cs
@@ -0,0 +1,16 @@
+﻿using Mafi.Base;
+using Mafi.Core.Research;
+using ResNodeID = Mafi.Core.Research.ResearchNodeProto.ID;
+
+namespace ExampleMod {
+	public partial class MyIds {
+
+		public partial class Research {
+
+			[ResearchCosts(difficulty: 1)]
+			public static readonly ResNodeID UnlockMyModStuff = Ids.Research.CreateId("UnlockMyModStuff");
+
+		}
+
+	}
+}
