c873c16b309585c43bcede7190e165a2937f0d97
MaFi Games <support@captain-of-industry.com>
2021-09-18 10:05:54 -0700

    Cleanup
    Moved sources to src

---
+++ src/MyMod.cs
@@ -0,0 +1,33 @@
+﻿using Mafi;
+using Mafi.Base;
+using Mafi.Core;
+using Mafi.Core.Mods;
+
+namespace ExampleMod {
+	public sealed class MyMod : DataOnlyMod {
+
+		public override string Name => "Example mod";
+
+
+		// Constructor that lists mod dependencies as parameters. You probably want to depend on both core and base
+		// mods so this mod will be initialized after them.
+		public MyMod(CoreMod coreMod, BaseMod baseMod) {
+			Log.Info("My mod was created!");
+		}
+
+		public override void RegisterPrototypes(ProtoRegistrator registrator) {
+			// Register all prototypes here.
+
+			// Registers all products from this assembly. See MyIds.Products.cs for examples.
+			registrator.RegisterAllProducts();
+
+			// Use data class registration to register other protos such as machines, recipes, etc.
+			registrator.RegisterData<ExampleRecipesData>();
+
+			// Registers all research from this assembly. See ExampleResearchData.cs for examples.
+			registrator.RegisterDataWithInterface<IResearchNodesData>();
+
+		}
+
+	}
+}
